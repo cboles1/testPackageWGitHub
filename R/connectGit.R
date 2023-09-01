befea236b050240b514b3a71677c0de4089e99ad
cboles1 <141677230+cboles1@users.noreply.github.com>
2023-09-01 01:58:59 -0400

    framing up a simple script that brings in important libraries and sets up git/github repo

---
+++ R/connectGit.R
@@ -0,0 +1,15 @@
+library(usethis)
+library(devtools)
+use_git()
+use_github(protocol = 'https')
+
+
+#A note about PATs and RStudio and GitHub - - note that this might be old
+# from: https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
+
+  # Now that you’ve created a Personal Access Token, we need to store it so that
+  # RStudio can access it and know to connect to your GitHub account. The gitcreds_set()
+  # function from the gitcreds package will help you here. You’ll enter your GitHub username
+  # and the Personal Access Token as your password (NOT your GitHub password, as I initially
+  # thought). Once you’ve done all of this, you have connected RStudio to GitHub!
+
