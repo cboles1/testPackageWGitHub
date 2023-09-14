c372810be8963c0efcd758addc5b487179c72b62
cboles1 <141677230+cboles1@users.noreply.github.com>
2023-09-14 17:37:16 -0400

    Initial commit

--- R/connectGit.R
+++ R/connectGit.R
@@ -5,3 +5,20 @@ use_github(protocol = 'https')
 use_readme_rmd()       #after running this command, you will need to go 'knit' the readme file,
 #                       might need to edit the example
 
+# EXAMPLE FUNCTION - Used so I can have a roxygen skeleton
+# Create a function to print squares of numbers in sequence.
+
+#' Title - print squares of numbers in sequence
+#'
+#' @param a Any number
+#'
+#' @return Squares of numbers 1 through a in sequence
+#' @export
+#'
+#' @examples
+new.function <- function(a) {
+  for(i in 1:a) {
+    b <- i^2
+    print(b)
+  }
+}
