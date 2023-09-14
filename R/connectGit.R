23dbda2ed3cbd965935f1f3dac1d47dacef34221
cboles1 <141677230+cboles1@users.noreply.github.com>
2023-09-14 15:47:07 -0400

    deleted the hello.R and associated Rd file
    updated the vignette file to be more of an "intro to using Git with R"
    
    removed extra info from connectGit.R and put that in the vignette

--- R/connectGit.R
+++ R/connectGit.R
@@ -5,13 +5,3 @@ use_github(protocol = 'https')
 use_readme_rmd()       #after running this command, you will need to go 'knit' the readme file,
 #                       might need to edit the example
 
-
-#A note about PATs and RStudio and GitHub - - note that this might be old
-# from: https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
-
-  # Now that you’ve created a Personal Access Token, we need to store it so that
-  # RStudio can access it and know to connect to your GitHub account. The gitcreds_set()
-  # function from the gitcreds package will help you here. You’ll enter your GitHub username
-  # and the Personal Access Token as your password (NOT your GitHub password, as I initially
-  # thought). Once you’ve done all of this, you have connected RStudio to GitHub!
-
